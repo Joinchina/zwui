9d57032a8f911f1c25c020b530c680f1b385abbc
unknown <zhangst@wanhuhealth.com>
2020-08-07 17:08:07 +0800

    完成alert组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Button from './components/Button/button'
+import Alert from './components/Alert/alert'
 const App: React.FC = ()=> {
   return (
     <div>
       <Button disabled >小明</Button>
       <Button onClick={()=>{alert(1)}} btnType="primary">小明</Button>
       <Button href="www.baidu.com" btnType="link">小明</Button>
+      <Alert message="我是title" description="我是描述" baseType="warning"></Alert>
+      <Alert message="我是title"  baseType="info"></Alert>
     </div>
   );
 }
--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -1,31 +1,46 @@
-import React, {  FC, ButtonHTMLAttributes, AnchorHTMLAttributes} from 'react'
+import React, { ButtonHTMLAttributes, AnchorHTMLAttributes, useState, useEffect } from 'react'
 import classnames from 'classnames'
 
 export type AlertType = 'success' | 'error' | 'warning' | 'info'
 interface AlertProps  {
     message?: string,
     description?: string,
-    type?: AlertType
+    baseType?: AlertType
 }
-const Alert: React.FC<AlertProps> = (props) => {
-    const {message,description,type} = props
+type NativeAlertProps = AlertProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorAlertProps = AlertProps & AnchorHTMLAttributes<HTMLElement>
+export type baseProps = Partial<NativeAlertProps & AnchorAlertProps>
+
+const Alert: React.FC<baseProps> = (props) => {
+    const {message,description,baseType} = props
     const classes = classnames('alert',{
-        [`alert-${type}`]:type,
+        [`alert-${baseType}`]:baseType,
     })
+    //定时器三秒隐藏
+    const [showAlert,setShow] = useState(true);
+    useEffect(() => {
+        setTimeout(()=>{
+            setShow(false)
+        },3000)        
+    },[showAlert])
     return (
-        <div className="alert_box">
-            <div className={classes}>
-                <h1>{message}</h1>
-                {description?<h2>{description}</h2>:null}
-            </div>
-            <div className={classes}>
-                x
-            </div>
-        </div>
+        <>
+            {
+                showAlert?<div className="alert_box">
+                    <div className={classes}>
+                        <h1>{message}<span onClick={()=>{
+                            setShow(false)
+                        }}>x</span></h1>
+                        {description?<h2>{description}</h2>:null}
+                    </div>
+                </div>:null
+            }
+        </>
     )
 }
+
 Alert.defaultProps = {
     message:'',
-    type:'info'
+    baseType:'info'
 }
 export default Alert
